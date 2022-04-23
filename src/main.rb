e3c0e79ebb52fe1096fa5e8746fe1b3cfb2e3b67
nhithutran <nhithutran@gmail.com>
2022-04-23 17:50:05 +1000

    Added new Plant-Watering-Tracker repo

---
+++ src/main.rb
@@ -0,0 +1,65 @@
+require 'colorize'
+require 'tty-prompt'
+require 'rubocop'
+require 'tty-font'
+require_relative './plant.rb'
+# require_relative './next_watering-date.rb'
+
+prompt = TTY::Prompt.new
+font = TTY::Font.new(:straight)
+
+puts '------------------------------------------------------------------'
+puts font.write('PLANT WATERING TRACKER').green.bold
+puts 'By Nhi Tran owner of 64 indoor plants'.green
+puts "------------------------------------------------------------------\n\n\n"
+
+array_of_pwl = []
+
+loop do # Main Application Loop - menu options
+  puts 'Welcome to the Plant Watering Tracker!' # Welcome message displaying plant watering tracker menu options using TTY-Prompt gem
+  menu = prompt.select('Please select from the following options:') do |choice|
+    choice.choice 'Add a new plant' # When user select this choice user asked what plant name
+    choice.choice 'Existing plants list'
+    choice.choice 'View plant next watering date'
+    choice.choice 'Exit'
+  end
+
+  puts menu
+  if menu == 'Add a new plant'
+    plant_name = prompt.ask('Please enter in your plant name?')
+    # array_of_pwl.<<(plant_name)
+    category = prompt.select('Please select the plant category?') do |choice|
+      choice.choice 'Cacti or Succulent'
+      choice.choice 'Foliage'
+      choice.choice 'Palm'
+      choice.choice 'Flowering'
+      # array_of_pwl.<<(category)
+    end
+    last_watered = prompt.ask('When did you last water the plant? Please enter a date in the following format DD/MM/YYY.')
+    # array_of_pwl.<<(last_watered)
+  
+  pwl = Plant.new(plant_name, category, last_watered) # Confirm to user their input
+    array_of_pwl.<<pwl  
+  puts "The plant you have added is a #{plant_name} in the category of #{category} and last watered #{last_watered}.".green
+    elsif menu == 'Existing plants list'
+        # for pwl in array_of_pwl pwl.display_nice_record
+        array_of_pwl.each do |p|  
+        p.display_nice_record
+    end  
+          
+  end  
+
+  # elsif menu == 'View plant next watering date'
+     
+    # array_of_pwl.each do |p| # ADD CALC take array_of_pwl catefory and date. If category cactus next plant watering date is 1 month if not 1 week
+    #  puts p.describe
+  # end
+#  else menu == 'Exit'Hmmm 
+#     exit 0
+
+# Handle no input
+# Exit loop if user types in a plant name? HOW TO EXIT LOOP?
+# if plant_name != ""
+# else
+# exit = 0
+  end
---
+++ src/plant_watering_log.rb
@@ -0,0 +1,14 @@
+class PlantWateringLog
+    # ?User to be able to input name, category and last watered date.  Should :name & :category be
+    attr_reader :plant_name, :category, :last_watered
+
+    def initialize(plant_name, category, last_watered)
+        @plant_name = plant_name
+        @category = category
+        @last_watered = last_watered
+    end
+
+    def display_nice_record # Method to display inputed by user nicely
+      puts plant_name() + '' + category() + '' + last_watered() + ''
+    end
+end
